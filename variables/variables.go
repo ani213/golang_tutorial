08cec487ef45686fa74a17cd793062c2e1665804
ani213 <vermaaniket213@gmail.com>
2025-01-02 15:06:57 +0530

    varibles declearation complete

--- variables/variables.go
+++ variables/variables.go
@@ -5,6 +5,27 @@ import "fmt"
 func main() {
 	var name string = "Golang"
 	fmt.Println(name)
+
+	// go lang auto infer
 	var name1 = "Golang"
 	fmt.Println(name1)
+
+	// int
+	var age int = 5
+	fmt.Println(age)
+
+	// bool
+	var isAdult bool = true
+	fmt.Println(isAdult)
+
+	var price = 4.7
+	fmt.Println((price))
+
+	// shorthand syntex
+	nameShortHand := "Golang"
+	fmt.Println((nameShortHand))
+	// assign after
+	var lastName string
+	lastName = "Verma"
+	fmt.Println(lastName)
 }
