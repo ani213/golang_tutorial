3189d9fad5fbc901414c1f0e7c58d3e096477707
ani213 <vermaaniket213@gmail.com>
2025-02-05 13:41:02 +0530

    pointers added

---
+++ Pointer/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var ptr *int
+	// fmt.Println("var value pointer", ptr) // o/p <nil>
+
+	myNumber := 1
+	var ptr = &myNumber
+
+	fmt.Println("vlue of ptr", ptr)
+	fmt.Println("vlue of ptr", *ptr)
+
+	*ptr = *ptr + 1
+	fmt.Println("mynumber value", myNumber)
+
+	modifiedMynumberValueByAddress(&myNumber)
+	fmt.Println("value is", myNumber)
+}
+
+func modifiedMynumberValueByAddress(num *int) {
+	*num = *num + 1
+}
